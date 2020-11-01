f2283a6e048de379501c2f26d58f928d331ef297
skydreamweb <info@skydream.info>
2020-11-01 19:54:36 +0100

    Update Index.vue
    Photos list

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -9,7 +9,7 @@
       style="max-width: 300px"
     >
       <q-file
-         clearable
+        clearable
         multiple
         filled
         color="purple-12"
@@ -19,6 +19,36 @@
       />
       <q-btn push color="teal" label="Add image" @click="addImage" />
     </div>
+    <q-separator spaced inset vertical dark />
+
+    <div class="text-h2 text-center q-my-lg">Uploaded photos</div>
+    <div class="row">
+    <q-list class="col-6" v-for="img in images" :key="img.imageId" bordered>
+      <q-item class="block" clickable v-ripple>
+        <q-item-section>
+          <img :src="img.src" />
+        </q-item-section>
+        <q-item-section class="flex">
+          <q-btn
+            icon="add"
+            push
+            color="primary"
+            label="Poster"
+            @click="addPoster"
+          />
+          <q-btn
+            icon="delete"
+            push
+            color="negative"
+            label="Delete"
+            @click="deleteImage"
+          />
+        </q-item-section>
+        
+      </q-item>
+    </q-list>
+    </div>
+
   </q-page>
 </template>
 
@@ -35,11 +65,9 @@ export default {
   },
   methods: {
     addImage() {
-
-      // FileReader read the contents of files 
+      // FileReader read the contents of files
       const reader = new FileReader();
       reader.addEventListener("load", () => {
-
         // Get next ID
         let imageId = 1;
         if (this.images.length > 0) {
@@ -53,14 +81,36 @@ export default {
         this.$q.localStorage.remove("images");
         this.$q.localStorage.set("images", JSON.stringify(this.images));
       });
-      // Read imagea
+
+      // Read images
       reader.readAsDataURL(this.image[0]);
 
       // Remove image from q-file
       this.image = null;
+      console.log(this.images);
+    },
+
+    addPoster(){
+      console.log("Image to Poster Added");
+    },
+    deleteImage(){
+      console.log("Deleted");
     }
-  }, mounted(){
-     this.images = getLSData("images");
+  },
+  mounted() {
+    // load data from localstorage after reload component
+    let images = JSON.parse(this.$q.localStorage.getItem("images"))
+      ? JSON.parse(this.$q.localStorage.getItem("images"))
+      : [];
+    if (images.length > 0) {
+      this.images = images;
+    }
   }
 };
 </script>
+
+<style lang="scss" scoped>
+img {
+  width: 100%;
+}
+</style>

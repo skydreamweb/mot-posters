7b912b3c0a7773a84bce9677b47fdbe4f48ea70f
skydreamweb <info@skydream.info>
2020-11-01 21:15:18 +0100

    Poster Create
    Component

---
+++ src/components/PosterCreator.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="block">
+        <div class="text-h2 text-center q-my-lg">Posters</div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "PosterCreator"
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -19,52 +19,81 @@
       />
       <q-btn push color="teal" label="Add image" @click="addImage" />
     </div>
+
+    <!-- Dialog if no image selected -->
+    <q-dialog v-model="alert">
+      <q-card>
+        <q-card-section>
+          <div class="text-h6">No image</div>
+        </q-card-section>
+
+        <q-card-section class="q-pt-none">
+          Please upload image to continue!
+        </q-card-section>
+
+        <q-card-actions align="right">
+          <q-btn flat label="OK" color="primary" v-close-popup />
+        </q-card-actions>
+      </q-card>
+    </q-dialog>
+
     <q-separator spaced inset vertical dark />
 
     <div class="text-h2 text-center q-my-lg">Uploaded photos</div>
     <div class="row">
-    <q-list class="col-6" v-for="img in images" :key="img.imageId" bordered>
-      <q-item class="block" clickable v-ripple>
-        <q-item-section>
-          <img :src="img.src" />
-        </q-item-section>
-        <q-item-section class="flex">
-          <q-btn
-            icon="add"
-            push
-            color="primary"
-            label="Poster"
-            @click="addPoster"
-          />
-          <q-btn
-            icon="delete"
-            push
-            color="negative"
-            label="Delete"
-            @click="deleteImage"
-          />
-        </q-item-section>
-        
-      </q-item>
-    </q-list>
+      <q-list class="col-6" v-for="img in images" :key="img.imageId">
+        <q-item class="block image-border" clickable v-ripple>
+          <q-item-section>
+            <img :src="img.src" />
+          </q-item-section>
+          <q-item-section class="block row text-center">
+            <q-btn
+              class="col q-ma-sm"
+              icon="add"
+              push
+              color="primary"
+              label="Poster"
+              @click="addPoster(img)"
+            />
+            <q-btn
+              class="col q-ma-sm"
+              icon="delete"
+              push
+              color="negative"
+              label="Delete"
+              @click="deleteImage(img.imageId)"
+            />
+          </q-item-section>
+        </q-item>
+      </q-list>
     </div>
-
+    <poster-creator></poster-creator>
   </q-page>
 </template>
 
 <script>
 import { getLSData } from "src/helpers/helperFunctions";
+import PosterCreator from "../components/PosterCreator";
 
 export default {
   name: "PageIndex",
+  components: {
+    PosterCreator
+  },
   data() {
     return {
+      alert: false,
       image: null, // q-file v-modal
-      images: [] // LocalStorage array
+      images: [], // LocalStorage array
+      singleImage: null // Images to create poster
     };
   },
   methods: {
     addImage() {
+      if (this.image == null) {
+        this.alert = true;
+        return;
+      }
       // FileReader read the contents of files
       const reader = new FileReader();
       reader.addEventListener("load", () => {
@@ -90,11 +119,21 @@ export default {
       console.log(this.images);
     },
 
-    addPoster(){
+    addPoster(img) {
       console.log("Image to Poster Added");
+      this.singleImage = img;
     },
-    deleteImage(){
+
+    // Delete image
+    deleteImage(imageId) {
+      // Update images array
+      this.images = this.images.filter(image => image.imageId !== imageId);
+
+      // Update localstorage
+      localStorage.removeItem("images");
+      localStorage.setItem("images", JSON.stringify(this.images));
       console.log("Deleted");
+      this.confirm = false;
     }
   },
   mounted() {
@@ -112,5 +151,10 @@ export default {
 <style lang="scss" scoped>
 img {
   width: 100%;
+}
+.image-border {
+  border-radius: 40px;
+  border: 1px solid black;
+  padding: 15px;
 }
 </style>

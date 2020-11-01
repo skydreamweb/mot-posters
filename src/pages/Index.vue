9c677581b20d4b4a15954e344f76f7cdc6d35eda
skydreamweb <info@skydream.info>
2020-11-01 16:22:04 +0100

    Update Index.vue
    Q-file size limit

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,9 +1,9 @@
 <template>
   <q-page class="block">
     <div class="text-h2 text-center q-my-lg">Please upload photo</div>
-    <div class="text-subtitle1 text-center">Select image to upload</div>
+    <div class="text-subtitle1 text-center">Select image to upload up to 512kb</div>
      <div class="q-gutter-y-md column" style="max-width: 300px">
-      <q-file clearable filled color="purple-12" v-model="image" label="Upload image" />
+      <q-file clearable filled color="purple-12" v-model="image" max-file-size="512" label="Upload image" />
      </div>
     <button @click="check">Check</button>
   </q-page>
